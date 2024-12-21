662f7b59f97dd8f2572e8ae4c34ab92a6b7cc336
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2024-12-21 17:57:27 -0500

    ENH: Provide appropriate type hints to `reg_target_type`
    Provide appropriate type hints to the `reg_target_type` parameter of the
    `ants._run_registration` function. The expression is set to a tuple in
    https://github.com/nipreps/nifreeze/blob/6b6ba707c47b2763c040f3d3a2ceabdcf450eefe/src/nifreeze/estimator.py#L103
    
    Fixes:
    ```
    src/nifreeze/registration/ants.py:459: error:
     Incompatible types in assignment (expression has type "tuple[str, str]", variable has type "str")  [assignment]
    ```
    
    raised for example in:
    https://github.com/nipreps/nifreeze/actions/runs/12437972140/job/34728973936#step:8:133

--- src/nifreeze/registration/ants.py
+++ src/nifreeze/registration/ants.py
@@ -413,7 +413,7 @@ def _run_registration(
     i_iter: int,
     vol_idx: int,
     dirname: Path,
-    reg_target_type: str,
+    reg_target_type: str | tuple[str, str],
     align_kwargs: dict,
 ) -> nt.base.BaseTransform:
     """
@@ -443,7 +443,7 @@ def _run_registration(
         DWI frame index.
     dirname : :obj:`Path`
         Directory name where the transformation is saved.
-    reg_target_type : :obj:`str`
+    reg_target_type : :obj:`str` or tuple of :obj:`str`
         Target registration type.
     align_kwargs : :obj:`dict`
         Parameters to configure the image registration process.
