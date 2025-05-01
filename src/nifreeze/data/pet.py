600be41f9c3a3399b9ce708ba26c2a13a4b88c45
mnoergaard <martin.noergaard@nru.dk>
2025-04-30 21:48:46 -0400

    FIX: update PET data model to fix total_duration

--- src/nifreeze/data/pet.py
+++ src/nifreeze/data/pet.py
@@ -189,9 +189,9 @@ class PET(BaseDataset[np.ndarray | None]):
         return cls(**data)
 
     def load(
-            filename,
-            json_file,
-            brainmask_file=None
+        filename,
+        json_file,
+        brainmask_file=None
     ):
         """Load PET data."""
         filename = Path(filename)
@@ -213,6 +213,7 @@ class PET(BaseDataset[np.ndarray | None]):
         midframe = frame_times_start + frame_duration / 2
 
         retval.midframe = midframe
+        retval.total_duration = float(frame_times_start[-1] + frame_duration[-1])
 
         assert len(retval.midframe) == retval.dataobj.shape[-1]
 
