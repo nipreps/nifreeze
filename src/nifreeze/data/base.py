fix: remove resampling from update step (and rotation of bvecs)

src/nifreeze/data/base.py
@@ -24,7 +24,6 @@
 
 from __future__ import annotations
 
-from collections import namedtuple
 from pathlib import Path
 from tempfile import mkdtemp
 from typing import Any, Generic
@@ -34,7 +33,6 @@ import h5py
 import nibabel as nb
 import numpy as np
 from nibabel.spatialimages import SpatialHeader
-from nitransforms.linear import Affine
 from typing_extensions import Self, TypeVarTuple, Unpack
 
 Ts = TypeVarTuple("Ts")
@@ -162,7 +160,7 @@ class BaseDataset(Generic[Unpack[Ts]]):
         """Get the filepath of the HDF5 file."""
         return self._filepath
 
-    def set_transform(self, index: int, affine: np.ndarray, order: int = 3) -> None:
+    def set_transform(self, index: int, affine: np.ndarray) -> None:
         """
         Set an affine transform for a particular index and update the data object.
 
@@ -172,36 +170,13 @@ class BaseDataset(Generic[Unpack[Ts]]):
             The volume index to transform.
         affine : :obj:`numpy.ndarray`
             The 4x4 affine matrix to be applied.
-        order : :obj:`int`, optional
-            The order of the spline interpolation.
 
         """
-        ImageGrid = namedtuple("ImageGrid", ("shape", "affine"))
-        reference = ImageGrid(shape=self.dataobj.shape[:3], affine=self.affine)
-
-        xform = Affine(matrix=affine, reference=reference)
-
-        if not Path(self._filepath).exists():
-            self.to_filename(self._filepath)
-
-        # read original DWI data & b-vector
-        with h5py.File(self._filepath, "r") as in_file:
-            root = in_file["/0"]
-            dataframe = np.asanyarray(root["dataobj"][..., index])
-
-        datamoving = nb.Nifti1Image(dataframe, self.affine, None)
-
-        # resample and update orientation at index
-        self.dataobj[..., index] = np.asanyarray(
-            xform.apply(datamoving, order=order).dataobj,
-            dtype=self.dataobj.dtype,
-        )
-
         # if head motion affines are to be used, initialized to identities
         if self.motion_affines is None:
-            self.motion_affines = np.repeat(np.zeros((4, 4))[None, ...], len(self), axis=0)
+            self.motion_affines = np.repeat(np.eye(4)[None, ...], len(self), axis=0)
 
-        self.motion_affines[index] = xform.matrix
+        self.motion_affines[index] = affine
 
     def to_filename(
         self, filename: Path | str, compression: str | None = None, compression_opts: Any = None
@@ -253,6 +228,24 @@ class BaseDataset(Generic[Unpack[Ts]]):
             The output NIfTI file path.
 
         """
+
+        # TODO: Resample if motion_affines is not None
+        # if not Path(self._filepath).exists():
+        #     self.to_filename(self._filepath)
+
+        # # read original DWI data & b-vector
+        # with h5py.File(self._filepath, "r") as in_file:
+        #     root = in_file["/0"]
+        #     dataframe = np.asanyarray(root["dataobj"][..., index])
+
+        # datamoving = nb.Nifti1Image(dataframe, self.affine, None)
+
+        # # resample and update orientation at index
+        # self.dataobj[..., index] = np.asanyarray(
+        #     xform.apply(datamoving, order=order).dataobj,
+        #     dtype=self.dataobj.dtype,
+        # )
+
         nii = nb.Nifti1Image(self.dataobj, self.affine, self.datahdr)
         if self.datahdr is None:
             nii.header.set_xyzt_units("mm")

src/nifreeze/data/dmri.py
@@ -24,7 +24,6 @@
 
 from __future__ import annotations
 
-from collections import namedtuple
 from pathlib import Path
 from typing import Any
 from warnings import warn
@@ -35,7 +34,6 @@ import nibabel as nb
 import numpy as np
 import numpy.typing as npt
 from nibabel.spatialimages import SpatialImage
-from nitransforms.linear import Affine
 from typing_extensions import Self
 
 from nifreeze.data.base import BaseDataset, _cmp, _data_repr
@@ -172,41 +170,6 @@ class DWI(BaseDataset[np.ndarray | None]):
         ]
         return list(zip(bval_estimated, indices, strict=True))
 
-    def set_transform(self, index: int, affine: np.ndarray, order: int = 3) -> None:
-        """
-        Set an affine transform for a particular index and update the data object.
-
-        The new affine is set as in :obj:`~nifreeze.data.base.BaseDataset.set_transform`,
-        and, in addition, the corresponding gradient vector is rotated.
-
-        Parameters
-        ----------
-        index : :obj:`int`
-            The volume index to transform.
-        affine : :obj:`numpy.ndarray`
-            The 4x4 affine matrix to be applied.
-        order : :obj:`int`, optional
-            The order of the spline interpolation.
-
-        """
-        if not Path(self._filepath).exists():
-            self.to_filename(self._filepath)
-
-        ImageGrid = namedtuple("ImageGrid", ("shape", "affine"))
-        reference = ImageGrid(shape=self.dataobj.shape[:3], affine=self.affine)
-
-        xform = Affine(matrix=affine, reference=reference)
-        bvec = self.bvecs[:, index]
-
-        # invert transform transform b-vector and origin
-        r_bvec = (~xform).map([bvec, (0.0, 0.0, 0.0)])
-        # Reset b-vector's origin
-        new_bvec = r_bvec[1] - r_bvec[0]
-        # Normalize and update
-        self.gradients[:-1, index] = new_bvec / np.linalg.norm(new_bvec)
-
-        super().set_transform(index, affine, order)
-
     def to_filename(
         self,
         filename: Path | str,
@@ -269,6 +232,20 @@ class DWI(BaseDataset[np.ndarray | None]):
 
         bvals = self.bvals
         bvecs = self.bvecs
+
+        # TODO Rotate bvectors if self.motion_affines is not None
+        # ImageGrid = namedtuple("ImageGrid", ("shape", "affine"))
+        # reference = ImageGrid(shape=self.dataobj.shape[:3], affine=self.affine)
+
+        # xform = Affine(matrix=affine, reference=reference)
+        # bvec = self.bvecs[:, index]
+
+        # # invert transform transform b-vector and origin
+        # r_bvec = (~xform).map([bvec, (0.0, 0.0, 0.0)])
+        # # Reset b-vector's origin
+        # new_bvec = r_bvec[1] - r_bvec[0]
+        # # Normalize and update
+        # self.bvecs[:, index] = new_bvec / np.linalg.norm(new_bvec)
 
         if self.bzero is None or not insert_b0:
             if insert_b0:

test/test_data_base.py
@@ -92,7 +92,7 @@ def test_set_transform(random_dataset: BaseDataset):
     data_before = np.copy(random_dataset.dataobj[..., idx])
     # Identity transform
     affine = np.eye(4)
-    random_dataset.set_transform(idx, affine, order=1)
+    random_dataset.set_transform(idx, affine)
 
     # Data shouldn't have changed (since transform is identity).
     volume0, aff0 = random_dataset[idx]  # type: ignore[misc]  # PY310