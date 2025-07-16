f9e354aa535a6979bd520ad89cd0e11751cac1d5
Oscar Esteban <code@oscaresteban.es>
2025-07-16 12:00:34 +0200

    fix: refactor ``test_main.py::test_main_call`` and capture warning

--- test/test_main.py
+++ test/test_main.py
@@ -52,9 +52,13 @@ def test_help(capsys):
         True,
     ],
 )
-def test_main_call(write_hdf5):
+@pytest.mark.filterwarnings("ignore:write_hmxfms is set to True")
+@pytest.mark.filterwarnings("error")
+def test_main_call(tmp_path, monkeypatch, write_hdf5):
+    """Test the main function of the CLI."""
+
+    os.chdir(tmp_path)
     called = {}
-    original_run = cli_run.Estimator.run
 
     # Define smoke run method
     def smoke_estimator_run(self, dataset, **kwargs):
@@ -62,7 +66,7 @@ def test_main_call(write_hdf5):
         called["kwargs"] = kwargs
 
     # Monkeypatch
-    cli_run.Estimator.run = smoke_estimator_run
+    monkeypatch.setattr(cli_run.Estimator, "run", smoke_estimator_run)
 
     input_file = Path(os.getenv("TEST_DATA_HOME")) / "dwi.h5"
     argv = [
@@ -73,24 +77,24 @@ def test_main_call(write_hdf5):
 
     if write_hdf5:
         argv.append("--write-hdf5")
-
-    try:
+        out_filename = "dwi.h5"
         cli_run.main(argv)
+    else:
+        out_filename = "dwi.nii.gz"
+        with pytest.warns(
+            UserWarning,
+            match="no motion affines were found",
+        ):
+            cli_run.main(argv)
 
-        out_filename = "dwi.h5" if write_hdf5 else "dwi.nii.gz"
-        output_dir = Path.cwd()
-
-        assert Path(output_dir / out_filename).is_file()
-        out_bval_filename = Path(Path(input_file).name).stem + ".bval"
-        out_bval_path: Path = Path(output_dir) / out_bval_filename
-        out_bvec_filename = Path(Path(input_file).name).stem + ".bvec"
-        out_bvec_path: Path = Path(output_dir) / out_bvec_filename
-        assert out_bval_path.is_file()
-        assert out_bvec_path.is_file()
-        if write_hdf5:
-            out_h5_filename = Path(Path(input_file).name).stem + ".h5"
-            out_h5_path: Path = Path(output_dir) / out_h5_filename
-            assert out_h5_path.is_file()
-
-    finally:
-        cli_run.run = original_run
+    assert Path(tmp_path / out_filename).is_file()
+    out_bval_filename = Path(Path(input_file).name).stem + ".bval"
+    out_bval_path: Path = Path(tmp_path) / out_bval_filename
+    out_bvec_filename = Path(Path(input_file).name).stem + ".bvec"
+    out_bvec_path: Path = Path(tmp_path) / out_bvec_filename
+    assert out_bval_path.is_file()
+    assert out_bvec_path.is_file()
+    if write_hdf5:
+        out_h5_filename = Path(Path(input_file).name).stem + ".h5"
+        out_h5_path: Path = Path(tmp_path) / out_h5_filename
+        assert out_h5_path.is_file()
