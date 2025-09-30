03e9ac47d9216b313d537bfb1665879f79bfc9f8
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2025-09-30 19:49:46 -0400

    TST: Exercise reading/writing PET data from/to files
    Exercise reading/writing PET data from/to files.

--- test/test_data_pet.py
+++ test/test_data_pet.py
@@ -22,6 +22,7 @@
 #
 
 import json
+from pathlib import Path
 
 import nibabel as nb
 import numpy as np
@@ -52,6 +53,15 @@ def random_dataset(setup_random_pet_data) -> PET:
     )
 
 
+@pytest.fixture
+def random_nifti_file(tmp_path, setup_random_uniform_spatial_data) -> Path:
+    _data, _affine = setup_random_uniform_spatial_data
+    _filename = tmp_path / "random_pet.nii.gz"
+    _img = nb.Nifti1Image(_data, _affine)
+    _img.to_filename(_filename)
+    return _filename
+
+
 @pytest.mark.parametrize("stat_func", (np.sum, np.mean, np.std))
 def test_compute_uptake_statistic(stat_func):
     rng = np.random.default_rng(12345)
@@ -71,6 +81,72 @@ def test_from_nii_requires_frame_time(setup_random_uniform_spatial_data, tmp_pat
 
     with pytest.raises(RuntimeError, match="frame_time must be provided"):
         from_nii(fname)
+
+
+@pytest.mark.parametrize(
+    ("brainmask_file", "frame_time", "frame_duration"),
+    [
+        (None, [0.0, 5.0], [5.0, 5.0]),
+        ("mask.nii.gz", [0.0, 5.0], [5.0, 5.0]),
+    ],
+)
+def test_from_nii(tmp_path, random_nifti_file, brainmask_file, frame_time, frame_duration):
+    filename = random_nifti_file
+    img = nb.load(filename)
+    if brainmask_file:
+        mask_data = np.ones(img.get_fdata().shape[:-1], dtype=bool)
+        mask_img = nb.Nifti1Image(mask_data.astype(np.uint8), img.affine)
+        mask_img.to_filename(brainmask_file)
+
+    pet_obj = from_nii(
+        filename,
+        brainmask_file=brainmask_file,
+        frame_time=frame_time,
+        frame_duration=frame_duration,
+    )
+    assert isinstance(pet_obj, PET)
+    assert pet_obj.dataobj.shape == img.get_fdata().shape
+    np.testing.assert_array_equal(pet_obj.affine, img.affine)
+    np.testing.assert_allclose(
+        pet_obj.midframe, np.array(frame_time) + 0.5 * np.array(frame_duration)
+    )
+    assert pet_obj.total_duration == frame_time[-1] + frame_duration[-1]
+
+    if brainmask_file:
+        assert hasattr(pet_obj, "brainmask")
+        np.testing.assert_array_equal(pet_obj.brainmask, mask_data)
+
+
+@pytest.mark.random_pet_data(5, (4, 4, 4), np.asarray([10.0, 20.0, 30.0, 40.0, 50.0]), 60.0)
+def test_to_nifti(tmp_path, random_dataset):
+    out_filename = tmp_path / "random_pet_out.nii.gz"
+    random_dataset.to_nifti(str(out_filename))
+    assert out_filename.exists()
+    loaded_img = nb.load(str(out_filename))
+    assert np.allclose(loaded_img.get_fdata(), random_dataset.dataobj)
+    assert np.allclose(loaded_img.affine, random_dataset.affine)
+    units = loaded_img.header.get_xyzt_units()
+    assert units[0] == "mm"
+
+
+@pytest.mark.parametrize(
+    ("frame_time", "frame_duration"),
+    [
+        ([0.0, 5.0], [5.0, 5.0]),
+    ],
+)
+def test_round_trip(tmp_path, random_nifti_file, frame_time, frame_duration):
+    filename = random_nifti_file
+    img = nb.load(filename)
+    pet_obj = from_nii(filename, frame_time=frame_time, frame_duration=frame_duration)
+    out_fname = tmp_path / "random_pet_out.nii.gz"
+    pet_obj.to_nifti(out_fname)
+    assert out_fname.exists()
+    loaded_img = nb.load(out_fname)
+    np.testing.assert_array_equal(loaded_img.affine, img.affine)
+    np.testing.assert_allclose(loaded_img.get_fdata(), img.get_fdata())
+    units = loaded_img.header.get_xyzt_units()
+    assert units[0] == "mm"
 
 
 @pytest.mark.random_pet_data(5, (4, 4, 4), np.asarray([10.0, 20.0, 30.0, 40.0, 50.0]), 60.0)
